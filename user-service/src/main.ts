c2fd5a60e7a2c4232bec9d5cccd230f69e2608ef
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2025-08-25 09:56:43 -0400

    feat: configure user service grpc

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'user',
+        protoPath: join(__dirname, '../../proto/user.proto'),
+        url: '0.0.0.0:5001',
+      },
+    },
+  );
+
+  await app.listen();
+  console.log('User Service is listening on port 5001');
 }
 bootstrap();
